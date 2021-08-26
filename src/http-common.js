d4a1dd33345bb860772465260bcfe4d7a1d288b8
jonathanr123 <joni_la12_elnacio@hotmail.com>
2021-08-26 03:51:11 -0300

    Agregue lo del token simple para autenticacion

--- src/http-common.js
+++ src/http-common.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "http://localhost:8080/api",
   headers: {
-    "Content-type": "application/json"
+    "Content-type": "application/json",
+    "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjI5OTYzNDk3LCJqdGkiOiJhNzZlYWU0ZDEyOWE0MmFiOGUwMjVlYjYzN2VlYWE2ZCIsInVzZXJfaWQiOiJqb25hIn0._J-ROFmyAZZWqxKcagCddBZBOKDjXrlFHo2C7cV53Bk"
   }
 });
